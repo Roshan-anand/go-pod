86ead0b2e38c0c670d68e2000726fe5d409ac7b0
Roshan A <roshananand2005@gmail.com>
2025-07-16 18:36:40 +0530

    updated sidebar ui

--- frontend/src/components/dashboard/SideBar.tsx
+++ frontend/src/components/dashboard/SideBar.tsx
@@ -1,65 +1,96 @@
-import { GoSidebarCollapse, GoSidebarExpand } from "react-icons/go";
 import { CiHome } from "react-icons/ci";
 import { IoFolderOpenOutline } from "react-icons/io5";
-import { FaPodcast } from "react-icons/fa6";
-import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import { motion } from "motion/react";
+import { useSelector } from "react-redux";
+import type { StateT } from "@/providers/redux/store";
+import { FaPodcast, FaUser } from "react-icons/fa";
 import { useNavigate } from "@tanstack/react-router";
+
+const links = [
+  {
+    label: "Dashboard",
+    href: "/dashboard/home/",
+    icon: <CiHome className="icon-md" />,
+  },
+  {
+    label: "Projects",
+    href: "/dashboard/projects/",
+    icon: <IoFolderOpenOutline className="icon-md" />,
+  },
+];
+
+export const Logo = ({ ...props }) => {
+  return (
+    <div
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+      {...props}
+    >
+      <FaPodcast className="icon-md text-btn-sec" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium whitespace-pre text-black dark:text-white"
+      >
+        GO POD
+      </motion.span>
+    </div>
+  );
+};
+export const LogoIcon = ({ ...props }) => {
+  return (
+    <div
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+      {...props}
+    >
+      <FaPodcast className="icon-md text-btn-sec" />
+    </div>
+  );
+};
 
 const SideBar = () => {
   const navigate = useNavigate();
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const button = (name: string) => {
-    return (
-      <Button
-        className={`flex ${isCollapsed && "flex-col"} gap-2 rounded-sm `}
-        style={{
-          justifyContent: isCollapsed ? "center" : "flex-start",
-        }}
-        variant={
-          window.location.pathname.split("/")[2] === name ? "prime" : "default"
-        }
-        onClick={() => navigate({ to: `/dashboard/${name}/` })}
-      >
-        {React.createElement(
-          name === "projects" ? IoFolderOpenOutline : CiHome,
-          { className: isCollapsed ? "icon-md" : "icon-sm" }
-        )}
-
-        {!isCollapsed ? (
-          <p>{name.charAt(0).toUpperCase() + name.slice(1)}</p>
-        ) : (
-          <h6 className="text-[0.7rem] opacity-80">
-            {name.charAt(0).toUpperCase() + name.slice(1)}
-          </h6>
-        )}
-      </Button>
-    );
-  };
+  const { name, pic } = useSelector((state: StateT) => state.user);
+  const [open, setOpen] = useState(false);
 
   return (
-    <aside className={` min-w-[70px] ${!isCollapsed && "min-w-[210px]"}  flex flex-col gap-4`}>
-      <header className="mt-3 px-2 flex justify-evenly items-center">
-        {!isCollapsed && (
-          <>
-            <FaPodcast className="icon-sm" />
-            <span className="ml-1 mr-auto">GO POD</span>
-          </>
-        )}
-        <Button onClick={() => setIsCollapsed(!isCollapsed)}>
-          {isCollapsed ? (
-            <GoSidebarCollapse className="icon-md" />
+    <Sidebar open={open} setOpen={setOpen}>
+      <SidebarBody className="justify-between gap-10">
+        <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+          {open ? (
+            <Logo onClick={() => navigate({ to: "/" })} />
           ) : (
-            <GoSidebarExpand className="icon-sm" />
+            <LogoIcon onClick={() => navigate({ to: "/" })} />
           )}
-        </Button>
-      </header>
-      <main className="px-1 py-3 flex flex-col gap-3">
-        {button("home")}
-        {button("projects")}
-      </main>
-    </aside>
+          <div className="mt-8 flex flex-col gap-2">
+            {links.map((link, idx) => (
+              <SidebarLink
+                key={idx}
+                link={link}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <SidebarLink
+            link={{
+              label: name!,
+              href: "#",
+              icon: (
+                <div className="icon-md shrink-0 rounded-full">
+                  {pic ? (
+                    <img src={pic} className="size-full" alt="Avatar" />
+                  ) : (
+                    <FaUser className="size-full" />
+                  )}
+                </div>
+              ),
+            }}
+          />
+        </div>
+      </SidebarBody>
+    </Sidebar>
   );
 };
 
